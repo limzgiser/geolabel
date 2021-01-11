4bcfa71727a6ea92b0f56bec483c109fdc8601a7
limz <limzgiser@163.com>
2021-01-11 10:48:41 +0800

    '修改绘制工具style'

--- src/app/business/geo-label/sign/edit-tool/edit-tool.component.ts
+++ src/app/business/geo-label/sign/edit-tool/edit-tool.component.ts
@@ -28,6 +28,8 @@ import {
   offMapEvent,
 } from '../../utils/mapTool';
 import { remove } from 'lodash';
+import {drawStyles} from "./style";
+import {tools} from "./toolConfig";
 
 @Component({
   selector: 'lb-edit-tool',
@@ -67,38 +69,7 @@ export class EditToolComponent implements OnInit {
   mapboxDraw: MapboxDraw = null;
   selectIndex: number = -1;
   deletesub = null;
-  @Input() tools: Array<drawToolItem> = [
-    {
-      label: '点',
-      type: 'draw_point',
-      defaultImg: './assets/img/map/icon_mark_point.png',
-      selectImg: './assets/img/map/icon_mark_point_selected.png',
-    },
-    {
-      label: '线',
-      type: 'draw_line_string',
-      defaultImg: './assets/img/map/icon_mark_line.png',
-      selectImg: './assets/img/map/icon_mark_line_selected.png',
-    },
-    {
-      label: '面',
-      type: 'draw_polygon',
-      defaultImg: './assets/img/map/icon_mark_rectangle.png',
-      selectImg: './assets/img/map/icon_mark_rectangle_selected.png',
-    },
-    {
-      label: '圆',
-      type: 'drag_circle',
-      defaultImg: './assets/img/map/icon_mark_circle.png',
-      selectImg: './assets/img/map/icon_mark_circle_selected.png',
-    },
-    // {
-    //   label: '矩形',
-    //   type:'draw_rectangle',
-    //   defaultImg:"./assets/img/map/icon_mark_circle.png",
-    //   selectImg:"./assets/img/map/icon_mark_circle_selected.png"
-    // },
-  ];
+  @Input() tools: Array<drawToolItem> =tools;
   eventCallBack = {};
   constructor(
     private mapboxmapService: MapboxmapService,
@@ -139,6 +110,7 @@ export class EditToolComponent implements OnInit {
       }, 20);
     }
     this.mapboxDraw = new MapboxDraw({
+      styles: drawStyles,
       modes: {
         ...MapboxDraw.modes,
         draw_circle: CircleMode,
---
+++ src/app/business/geo-label/sign/edit-tool/toolConfig.ts
@@ -0,0 +1,32 @@
+export const tools =[
+  {
+    label: '点',
+    type: 'draw_point',
+    defaultImg: './assets/img/map/icon_mark_point.png',
+    selectImg: './assets/img/map/icon_mark_point_selected.png',
+  },
+  {
+    label: '线',
+    type: 'draw_line_string',
+    defaultImg: './assets/img/map/icon_mark_line.png',
+    selectImg: './assets/img/map/icon_mark_line_selected.png',
+  },
+  {
+    label: '面',
+    type: 'draw_polygon',
+    defaultImg: './assets/img/map/icon_mark_rectangle.png',
+    selectImg: './assets/img/map/icon_mark_rectangle_selected.png',
+  },
+  {
+    label: '圆',
+    type: 'drag_circle',
+    defaultImg: './assets/img/map/icon_mark_circle.png',
+    selectImg: './assets/img/map/icon_mark_circle_selected.png',
+  },
+  // {
+  //   label: '矩形',
+  //   type:'draw_rectangle',
+  //   defaultImg:"./assets/img/map/icon_mark_circle.png",
+  //   selectImg:"./assets/img/map/icon_mark_circle_selected.png"
+  // },
+]
