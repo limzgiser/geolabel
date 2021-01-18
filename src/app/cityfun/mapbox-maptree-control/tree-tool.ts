d9318b8e95f48592c7592f99de506467c7a4b553
limz <limzgiser@163.com>
2021-01-18 15:38:57 +0800

    对接平台服务

---
+++ src/app/cityfun/mapbox-maptree-control/tree-tool.ts
@@ -0,0 +1,16 @@
+export   function  addLayers(map,layers) {
+    layers.forEach(layerInfo => {
+        switch (layerInfo.type) {
+            // PT-VTStyle,PT-WMS,PT-WMTS,PT-ESRI-Tile,PT-ESRI-Dynamic
+            case "PT-WMS":
+                map.
+                break;
+        
+            default:
+                break;
+        }
+    });
+}   
+
+
+  
